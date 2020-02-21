fe29855395c672fd93bda60f452a62926ec4a1a1
CurtisJamesPhan <curtisjamesphan@gmail.com>
2020-02-20 18:19:12 -0800

    Updates

---
+++ precache-manifest.cdbe25830253d7b8fd7c6e4589ad57b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad1417a2da3248254e83eea1e5f809d",
+    "url": "/monstas/index.html"
+  },
+  {
+    "revision": "833a9b7d2c75a44a72c7",
+    "url": "/monstas/static/css/main.73e67267.chunk.css"
+  },
+  {
+    "revision": "cf1448c076a940264fc1",
+    "url": "/monstas/static/js/2.866fd185.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monstas/static/js/2.866fd185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833a9b7d2c75a44a72c7",
+    "url": "/monstas/static/js/main.36de3764.chunk.js"
+  },
+  {
+    "revision": "165d397d78ba01821890",
+    "url": "/monstas/static/js/runtime-main.f51a9b95.js"
+  }
+]);
